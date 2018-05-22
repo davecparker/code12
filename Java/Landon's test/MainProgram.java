c46bbcf896848ee18c81f58bd8eff6caa4b63307
Landon Fox <landonfox00@gmail.com>
2018-05-22 16:31:40 -0700

    New rectangle

---
+++ Java/Landon's test/MainProgram.java
@@ -0,0 +1,18 @@
+import Code12.*;
+
+public class MainProgram extends Code12Program
+{
+   public static void main(String[] args)
+   { 
+      Code12.run(new MainProgram()); 
+   }
+   
+   public void start()
+   {  
+      ct.rect(30, 50, 60, 60);
+   }
+   
+   public void update()
+   {        
+   }
+}
