141cf55609b3f52b344fe9e4199c66c3a237a49d
Ben Bennett-Cauchon <bcauchon@gmail.com>
2018-05-23 08:41:59 -0700

    Ducks added
    Ducks fly accross the window at random intervals

--- Java/Duck Hunt/MainProgram.java
+++ Java/Duck Hunt/MainProgram.java
@@ -22,11 +22,19 @@ public class MainProgram extends Code12Program
       
       // Make gun
       gun = ct.image( "gun.png", 50, 0, 8 );
-      gun.y = yMax - gun.height / 2;
+      setGunSizeAndPosition();
    }
    
    public void update()
    {
+      // Make ducks at random times and positions
+      if (ct.random(1, 50) == 1)
+      {
+         double x = 110;
+         double y = ct.random( 10, (int)(yMax / 2) );
+         GameObj duck = createDuck( x, y, -0.5 );
+      }
+      
       
    }
    
@@ -38,6 +46,16 @@ public class MainProgram extends Code12Program
       bullet.autoDelete = true;
       bullet.ySpeed = -2;
       return bullet;
+   }
+   
+   // Makes a duck to the right of the window at y-coordinate yStart
+   // that will then accross the window horizontally with speed xSpeed
+   public GameObj createDuck( double xStart, double yStart, double xSpeed )
+   {
+      GameObj duck = ct.image( "rubber-duck.png", xStart, yStart, 5 );
+      duck.autoDelete = true;
+      duck.xSpeed = xSpeed;
+      return duck;
    }
    
    // Moves the gun horizontally and first a bullet when the mouse
@@ -53,11 +71,27 @@ public class MainProgram extends Code12Program
       createBullet( xStart, yStart );
    }
    
+   // Sets an objects height to height and changes its width to preserve
+   // its original aspect ratio
+   public void setHeight( GameObj obj, double height )
+   {
+      double aspectRatio = obj.width / obj.height;
+      obj.height = height;
+      obj.width = height * aspectRatio;
+   }
+   
+   // Scales the gun so that it's height is 25% of the window height
+   // and sets it's position to the bottom of the window
+   public void setGunSizeAndPosition()
+   {
+      setHeight( gun, yMax * 0.25 );
+      gun.y = yMax - gun.height / 2;
+   }
+   
    // Updates yMax and image positions when the window is resized.
    public void onResize()
    {
       yMax = ct.getHeight();
-      gun.y = yMax - gun.height / 2;
+      setGunSizeAndPosition();
    }
-
 }
