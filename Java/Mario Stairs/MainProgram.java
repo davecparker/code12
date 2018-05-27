725ccf872799ac24ad24c99560f1633e2386c13e
Ben Bennett-Cauchon <bcauchon@gmail.com>
2018-05-27 16:01:16 -0700

    Mario Stairs First Commit
    Drew background.
    Drew ground and stairs using tiles and for loops.

---
+++ Java/Mario Stairs/MainProgram.java
@@ -0,0 +1,44 @@
+import Code12.*;
+
+public class MainProgram extends Code12Program
+{
+   public static void main(String[] args)
+   { 
+      Code12.run(new MainProgram()); 
+   }
+   
+   public void start()
+   {
+      // Make the background
+      ct.setBackColorRGB( 104, 136, 255 );
+      ct.image( "cloud.png", 25, 25, 19 );
+      
+      // Make the ground
+      double tileSize = 100 / 16;
+      int numberOfTiles = ct.toInt(100 / tileSize) + 1;
+      for (int i = 0; i < numberOfTiles; i++)
+      {
+         for (int j = 0; j < 2; j++)
+         {
+            GameObj tile = ct.image( "ground-tile.png", 100 - i * tileSize, 100 - j * tileSize, tileSize );
+            tile.align( "bottom right" );
+         }
+      }
+      
+      // Make the staircase
+      double yStairsBottom = 100 - tileSize * 2;
+      int numberOfLevels = 8;
+      for (int level = 0; level < numberOfLevels; level++)
+      {
+         for (int i = 0; i < numberOfLevels + 1 - level; i++)
+         {
+            GameObj block = ct.image( "block.png", 100 - i * tileSize, yStairsBottom - level * tileSize,  tileSize );
+            block.align( "bottom right" );
+         }
+      }
+   }
+   
+   public void update()
+   {        
+   }
+}
