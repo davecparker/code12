85ea5f7b9211dd8ebda77f845e3a73df961b7284
Nicole Monroy <38896487+alias-nicole@users.noreply.github.com>
2018-05-29 17:22:58 -0700

    Rudimentary Platformer
    In-progress

--- Java/Nicole Test/Platformer.java
+++ Java/Nicole Test/Platformer.java
@@ -1,12 +1,16 @@
-
 import Code12.*;
-
 import java.util.ArrayList;
 import java.util.Iterator;
+import javafx.animation.AnimationTimer;
 
-public class Platformer extends Code12Program
+class Platformer extends Code12Program
 {
-    public static final String[] LEVEL1 = new String[] 
+   public static void main(String[] args )
+   {
+      Code12.run(new Platformer()); 
+   }
+   
+   public final String[] LEVEL1 =  
     {
         "000000000000000000000000000000",
         "000000000000000000000000000000",
@@ -19,110 +23,158 @@ public class Platformer extends Code12Program
         "000000001110000000000000000000",
         "000002000000011100002000000000",
         "000001110000000000011100011000",
-        "111111110011110001111100111111"
+        "111111110011110001111100111111" // level one
     };
     
-   //array of platforms, maybe switch to tileset
-   ArrayList<GameObj> platforms = new ArrayList<GameObj>();
-   
-   GameObj player;
-   GameObj background;
-  
-   int levelWidth;
-   
-   static final int PLAYER_HEIGHT = 10;
-   static final int PLAYER_WIDTH = 10;
-   float vDelta;     // Vertical delta (changes over time)
-   float rebound; 
-   float reboundDeg; // Measured in degrees, how much rebound degrades over time
-   double yPos;      // The vertical position
-   float gravity; 
-        
-   boolean bounce = false;
 
-   
-   public static void main(String[] args)
-   { 
-      Code12.run(new Platformer()); 
-   }
-   
-   public void start()
-   {
-      ct.setHeight(100.0 * 9 / 16);
-      double width = ct.getWidth();
-      double height = ct.getHeight();
-      
-      background = ct.rect( width / 2, height / 2, width, height ); 
-      background.setFillColor("black");
-      
-      player = ct.image("blob.png", 0, height -3, 10 );
-      player.setSize(PLAYER_HEIGHT, PLAYER_WIDTH);
-    
-   }
-   
-   public void update()
-   {
-      //initialize
-      levelWidth = LEVEL1[0].length() * 20;  // Multiplier is for scaling(how big each part of the platform will be)
-      
-      for ( int i = 0; i < LEVEL1.length; i++ )
-      {
-         String line = LEVEL1[i];
-         
-         for ( int j = 0; j < line.length(); j++ )
-         {
-            switch ( line.charAt(j) )
+     ArrayList<GameObj> platforms = new ArrayList<GameObj>();
+     ArrayList<GameObj> coins = new ArrayList<GameObj>();
+     ArrayList<GameObj> enemies = new ArrayList<GameObj>();
+
+     GameObj player;
+     GameObj platform;
+     GameObj coin;
+     GameObj scoreText;
+     GameObj gameOverText;
+     GameObj timerText;
+     
+     int score;
+
+     double gravity = 0.5;
+     boolean onGround = false;
+     double levelWidth;
+
+
+    public void start()
+    {
+        // Background and static game features 
+        ct.setBackColor("black");
+        score = 0;
+        scoreText = ct.text("Score: ", 8, ct.getHeight() - 10, 5, "white");
+        timerText = ct.text("Time: " + 0, ct.getWidth() - 15, ct.getHeight() - 10, 5, "white");
+       
+        // Multiplier is used for scaling the platforms
+        double mult = ct.getWidth() / 30;
+        levelWidth = LEVEL1[0].length() * mult;      
+
+        for (int i = 0; i < 12; i++)   // This is so I can use the charAt method
+        {
+            String line = LEVEL1[i];
+            for (int j = 0; j < line.length(); j++)
             {
-               case '0':
-                  break;
-               case '1':
-                  GameObj platform = ct.rect(j*20,i*20,20,20);
-                  platform.setFillColor("white");
-                  platforms.add(platform);
-                  break;
-                  
-                   
+                switch (line.charAt(j))
+                {
+                    case '0':
+                        break;
+                    case '1':
+                         platform = ct.rect(j*mult, i*mult, mult, mult );
+                        
+                        platform.setFillColor("orange");
+                        platforms.add(platform);
+                        break;
+                    case '2':
+                        coin = ct.circle(j*mult, i*mult, mult/2);
+                        coin.setFillColor("yellow");
+                        coins.add(coin);
+                        break;
+                     // Maybe add case 3 for enemy ?
+                }
             }
+        }
+     
+        player = ct.rect(5, 34, 2, 2);
+        player.setFillColor("blue");
+        
+    }
+
+    public void update()
+    {
+      double time = ct.getTimer();
+      timerText.delete();
+      timerText = ct.text("Time: " + time, ct.getWidth() - 15, ct.getHeight() - 10, 5, "white");
+      
+      player.ySpeed += gravity;
+      player.y += player.ySpeed;
+      
+      for (GameObj platform: platforms )
+      {
+         if ( player.hit(platform) )
+         {
+            // check to see if player hit the platform from above or below
+            player.ySpeed = 0.0;
+            player.y = platform.y - (platform.height/2 +2); //slightly above
+            onGround = true;
          }
-         
+      }
+
+      /*Iterator<GameObj> it = coins.iterator();
+      while ( it.hasNext() )
+      {
+         GameObj coin = it.next();
+         if (coin.hit(player) )
+         {
+            coin.delete();          // Delete from screen
+     // Delete from ArrayList coins
+            score++;  
+         }
+      }*/
+      
+      for ( GameObj coin: coins )
+      {
+         if ( coin.hit(player) )
+         {
+            for ( int i = 0; i < coins.size() - 1; i++ )
+            {
+                  coins.remove(coins.get(i));
+            }
+            coin.delete();
+            score++;
+         }
+      
+      }
+      
+      
+      scoreText.delete();
+      scoreText = ct.text( "Score: " + Integer.toString(score), 12, ct.getHeight() - 10, 5, "white");
+      
+
+      if ( player.x >= ct.getWidth() || player.y >= ct.getHeight() )
+      {
+         ct.clearScreen();
+         gameOverText = ct.text("Game Over!", ct.getWidth() / 2, ct.getHeight() / 2, 10, "white");
+         gameOverText.ySpeed = gravity;
          
       }
       
-      //scrolling, will be called whenever the player's x position has changed..need to work on this
-      /*double x = player.x;
-      
-      x.addListener((obs, old, newValue) -> {          
-            double offset = newValue.intValue();
+    }
 
-            if (offset > 640 && offset < levelWidth - 640) {
-                gameRoot.setLayoutX(-(offset - 640));
-            }
-        });
-
-         appRoot.getChildren().addAll(background, gameRoot, uiRoot);   */
-         
-        
-
-   }
-   
-   public void onKeyPress( String key )
-   {
-      yPos = ct.getHeight() - PLAYER_HEIGHT;
-            vDelta = 0;
-            gravity = 0.25f;        // Can be adjusted
-            reboundDeg = 2.5f;      // Decreased each time player hits floor
-            
-      double height = ct.getHeight();
-      
-      switch ( key )
+  
+    public void startJump()
+    {
+      if ( onGround )
       {
-         case("right"):
-            if ( player.x > ct.getWidth() )
-               player.x = 0;
-            player.x += 5;
+            player.ySpeed = -6.0;
+            onGround = false;
+      }
+    }
+    
+    public void endJump()
+    {
+      if (player.ySpeed < -3.0)
+         player.ySpeed = -3.0;
+    }
+    
+   
+    public void onKeyPress( String key )
+    {
+      switch (key)
+      {
+         case("w"):
+             startJump();
+             endJump();
             break;
-         case("left"):
-            player.x -= 5;
+         case("d"):
+            player.x++;
             break;
          case("space"):
             if (yPos + PLAYER_HEIGHT == height )
@@ -163,11 +215,12 @@ public class Platformer extends Code12Program
 
             }
       }
-      
-      
-   }
+
+    }
+
 
 }
-                              
-                            
-                      
+
+
+ 
+  
