6bc2452c659389f090067b72687731af42f44ff9
Nicole Monroy <38896487+alias-nicole@users.noreply.github.com>
2018-05-31 13:50:15 -0700

    Fixed brush drawing + eraser abilities

--- Java/Nicole Use Case Tests/Paint.java
+++ Java/Nicole Use Case Tests/Paint.java
@@ -4,9 +4,8 @@ import Code12.*;
 public class Paint extends Code12Program
 {
    String currentColor = "blue";    // Default color
-   
    GameObj brush;
-
+   
    GameObj redRect;
    GameObj orangeRect;
    GameObj yellowRect;
@@ -46,9 +45,9 @@ public class Paint extends Code12Program
       violetRect = ct.rect(spacing,spacing*7 - offset, WIDTH, spacing, "purple");
       purpleRect = ct.rect(spacing,spacing*8 - offset, WIDTH, spacing, "dark majenta");
          
-      // This will be pressed to clear the canvas
+      // This will be pressed to activate the eraser
       eraseRect = ct.rect(spacing,spacing*9, WIDTH, spacing + 6, "white"); 
-      ct.text("Erase", spacing, spacing*9,5);
+      ct.text("Eraser", spacing, spacing*9,5);
       
       
    }
@@ -75,8 +74,7 @@ public class Paint extends Code12Program
    
    public void onMousePress( GameObj obj, double x, double y )
    {
-     //brush.group = "drawingsOnBoard";
-      
+
       if ( obj != null )
       {
             if ( obj == redRect )
@@ -93,31 +91,29 @@ public class Paint extends Code12Program
                currentColor = "dark blue";
             else if ( obj == purpleRect )
                currentColor = "purple";
-            //else if ( obj == eraseRect )
-               //ct.clearGroup("drawingsOnBoard");
+            else if ( obj == eraseRect )
+               currentColor =  "white";
       }
    }
    
    public void onMouseDrag( GameObj obj, double x, double y )
    {
-      int pixelWidth = ct.round( ct.getWidth() * ct.getPixelsPerUnit() );
-      int pixelHeight = ct.round( ct.getHeight() * ct.getPixelsPerUnit() );
        
       // Player can only draw on canvas
       if ( ct.clickX() > 20 )
       {
-         //brush = ct.line(ct.clickX(),ct.clickY(),x,y);
-         brush = ct.rect(ct.clickX(),ct.clickY(),0.25,0.25);
-         //brush.setLineColor(currentColor);
+         // If the eraser is active, set "brush" size slightly larger
+         if ( currentColor == "white" )
+         {
+            brush.lineWidth = 20;
+         }
+         brush = ct.line(ct.clickX(),ct.clickY(),x,y);
+         brush.setLineColor(currentColor);
          brush.setFillColor(currentColor);
-         // set restitution method?
+         brush.lineWidth = 10;
       }
       
       
-   }
-   
-   public void onMouseRelease( GameObj obj, double x, double y )
-   {
    }
    
    
