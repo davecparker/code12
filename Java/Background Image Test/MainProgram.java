b03cd01db26b7e0445f57530cd36cc62fe1dd66d
Ben Bennett-Cauchon <bcauchon@gmail.com>
2018-06-07 17:20:21 -0700

    Math & Misc Test - through canParseInt

--- Java/Background Image Test/MainProgram.java
+++ Java/Background Image Test/MainProgram.java
@@ -6,24 +6,31 @@ class MainProgram extends Code12Program
    { 
       Code12.run(new MainProgram()); 
    }
-   
-   int start;
+   int frameCount;
+   GameObj image;
    
    public void start()
    {  
-      ct.setBackImage( "background.png" );
-      start = ct.getTimer();
+      frameCount = 1;
+      double pixelsPerUnit = ct.getPixelsPerUnit();
+      double width = 225 / pixelsPerUnit;
+      double height = 224 / pixelsPerUnit;
       
+      image = ct.image( "background.png", width, height, 100 );
+      image.xSpeed = 1;
    }
    
    public void update()
    {  
-      int millisec = ct.getTimer() - start;
-      if ( millisec >= 60000 )  
-      {
-         ct.setBackImage( "background.png" );
-         start = millisec;
-         ct.println( "background set" );
-      }
+//       frameCount++;
+//       if ( image.width > 10 && frameCount % 10 == 1)
+//       {
+//          image.width -= 1;
+//          image.height -= 1;
+//       }
+         if (image.x > 100)
+         {
+            image.x = 0;
+         }
    }
 }
