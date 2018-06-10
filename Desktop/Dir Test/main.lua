d5051c4e05aabb428e037964b38137a4c69267fe
davecparker <dcparker65@gmail.com>
2018-06-10 16:00:42 -0700

    Trying Dir Test for Windows

--- Desktop/Dir Test/main.lua
+++ Desktop/Dir Test/main.lua
@@ -12,20 +12,58 @@ local yImageDir = 160
 
 --- Utility functions from Code12 app ----------------------------
 
--- Return a relative path in the file system leading from fromDir to destDir.
-local function relativePath( fromDir, destDir )
-	-- TODO: This is Mac-only for now and doesn't feel very reliable
-	local str, count = string.gsub( fromDir, "/", "." )
-	local upDirs = string.rep( "../", count )
-	print(count, upDirs)
-	return upDirs .. string.sub( destDir, 2 )
+local chDirSeperator
+if system.getInfo( "platform" ) == "win32" then
+	chDirSeperator = "\\"
+else
+	chDirSeperator = "/"
 end
 
--- Split a file pathname into dir, filename (includes ext), and ext.
--- Return (dir, filename, ext).
-local function dirFilenameAndExtFromPath( path )
-	-- TODO: Got this from internet, does it really work?
-	return string.match( path, "(.-)([^\\/]-%.?([^%.\\/]*))$" )
+-- Return a relative path in the file system leading from fromDir to destDir.
+local function relativePath( fromDir, destDir )
+	-- TODO: This is annoying and doesn't feel very reliable
+
+	-- Count the chDirSeperators in fromDir and create upDirs to go up to root
+	local str, count = string.gsub( fromDir, chDirSeperator, "." )
+	local up = ".." .. chDirSeperator
+	local upDirs = string.rep( up, count )
+	print(count, upDirs)
+
+	-- Remove the drive from destDir if any
+	-- TODO: This means it won't work across drives
+	if system.getInfo( "platform" ) == "win32" then
+		local iCharColon = string.find( destDir, ":" )
+		if iCharColon then
+			destDir = string.sub( destDir, iCharColon + 1 )
+		end
+	end
+
+	-- Remove root folder indicator from destDir if any
+	if string.sub( destDir, 1, 1 ) == chDirSeperator then
+		destDir = string.sub( destDir, 2 )
+	end
+
+	-- Attach the relative dir and return it
+	return upDirs .. destDir
+end
+
+-- Split a file pathname into dir and filename parts.
+-- Return (dir, filename). The dir includes the last / or \.
+local function dirAndFilenameOfPath( path )
+	-- Find the last / or \ if any
+	local iChar = string.find( path, chDirSeperator )
+	if iChar == nil then
+		return "", path
+	end
+	local iCharLast
+	while iChar do
+		iCharLast = iChar
+		iChar = string.find( path, chDirSeperator, iChar + 1 )
+	end
+
+	-- Split the path and return the parts
+	return string.sub( path, 1, iCharLast ), 
+			(string.sub( path, iCharLast + 1 ) or "")
 end
 
 
@@ -65,7 +103,10 @@ local function useOpenDialog()
 		resultGroup = showDir( yImageDir, "Chosen file", path )
 
 		-- Build the relative path and display the results
-		local dir, filename, ext = dirFilenameAndExtFromPath( path )
+		local dir, filename = dirAndFilenameOfPath( path )
+		print( "dir = \"" .. dir .. "\"" )
+		print( "filename = \"" .. filename .. "\"" )
+
 		showDir( yImageDir + 30, "File dir", dir )
 		local relDir = relativePath( system.pathForFile(nil, system.DocumentsDirectory), dir )
 		local relPath = relDir .. filename
