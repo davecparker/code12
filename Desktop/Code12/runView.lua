c45a7120b79e72c49932f2a274d1e1584f3e0f98
Ben Bennett-Cauchon <bcauchon@gmail.com>
2018-11-20 08:32:42 -0800

    Deleted print statement from runView.lua

--- Desktop/Code12/runView.lua
+++ Desktop/Code12/runView.lua
@@ -167,7 +167,6 @@ local function makeGrid()
 		n = n + 1
 		gridYLabels[n].isVisible = false
 	end
-	print("numXLabels", #gridXLabels, "numYLabels", #gridYLabels)
 end
 
 -- Position the display panes
