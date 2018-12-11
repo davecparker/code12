1a7d6928a439e1d5abb1ef94ae9d0dabefb6ad82
Ben Bennett-Cauchon <bcauchon@gmail.com>
2018-12-10 18:55:35 -0800

    Added new checkJava tests to ErrorTestCode.java
    Also:
    - Added more error tests involving instance variables
    - Added and moved some lines in ErrorQuestions.java
    - In parseJava.lua, modified console printing of syntax errors to include the line number.

--- Desktop/Error Test - checkJava/ErrorQuestions.java
+++ Desktop/Error Test - checkJava/ErrorQuestions.java
@@ -1,12 +1,35 @@
 class ErrorQuestions
 {
+	void testFunc()
+	{
+		// Can we generate something like "multidimentional arrays not supported"
+		// instead of 'Syntax Error: "[" was unexpected here' ?
+		int[][] multiDimArr = new int[3][4];
+
+		// Can second "(" be highlighted in the error?
+		// ERROR "Loop test must evaluate to a boolean"
+		while ( (1 + 1.0 == 2e0) + "")
+			break;
+
+		// Java allows empty array init?
+		int[] arrEmptyInit = { };
+
+		// Should "~" generate "operator is not supported" error instead of
+		// "~" was unexpected here?
+		ct.println(~2);
+
+		// Support "." after function call that returns a String or GameObj?
+		GameObj rect = ct.rect(50, 50, 10, 10);
+		boolean boolVar = rect.getText().equals("");
+
+		// Should this generate error "Syntax Error: a name was unexpected here" ?
+		Boolean boolVar = rect.getText().equals("");
+	}
+
 	// Generate error for not supporting variadic methods?
-	// void variadicMethod(int... args)
-	// {
-	// }
-	
-	// Java allows empty array init?
-	int[] arrEmptyInit = { };
+	void variadicMethod(int... args)
+	{
+	}
 
 	int funcMissingReturn5()
 	{
@@ -55,16 +78,6 @@ class ErrorQuestions
 
 	public void start()
 	{
-		// Should "~" generate "operator is not supported" error instead of
-		// "~" was unexpected here?
-		ct.println(~2);
-
-		// Support "." after function call that returns a String or GameObj?
-		GameObj rect = ct.rect(50, 50, 10, 10);
-		boolean boolVar = rect.getText().equals("");
-
-		// Should this generate error "Syntax Error: a name was unexpected here" ?
-		Boolean boolVar = rect.getText().equals("");
 	}
 
 	// ? error not generated
