e3147f3fbea5b83018cab4961cc9b6ebc84e7297
Ben Bennett-Cauchon <bcauchon@gmail.com>
2019-01-03 15:08:15 -0800

    DuckHunt6-8

--- Java/Duck Hunt/DuckHunt7.java
+++ Java/Duck Hunt/DuckHunt7.java
@@ -1,7 +1,7 @@
-// Syntax Level 6: Object Data Fields
+// Syntax Level 7: Object Method Calls
 // Draws the duckhunt background with a duck and the NES light gun.
 // Animates the duck moving accross the screen, the bomb falling, and the bullet moving up.
-class DuckHunt6
+class DuckHunt7
 {
 	final double ASPECT_RATIO = (double) 16 / 9; // width / height
 	final double GAME_HEIGHT = 100 / ASPECT_RATIO;
@@ -22,9 +22,9 @@ class DuckHunt6
 		double width = 6;
 		gun = ct.image( filename, x, y, width );
 		// Draw a bullet above the gun
-		x += 2;
-		y -= 10;
 		double diameter = 1;
+		x = gun.x + gun.getWidth() * 0.35;
+		y = gun.y - gun.getHeight() / 2 - diameter / 2;
 		String color = "black";
 		bullet = ct.circle( x, y, diameter, color );
 		// Draw a duck
@@ -36,22 +36,22 @@ class DuckHunt6
 		y = ct.random( yMinDuck, yMaxDuck );
 		duck = ct.image( filename, x, y, width );
 		// Draw a duck bomb below the duck
-		x = x - 5;
-		y = y + 5;
-		width = 0.5;
+		x = duck.x - duck.getWidth() / 2;
 		double height = 1.5;
+		y = duck.y + duck.getHeight() / 2;
+		width = 0.5;
 		color = "white";
 		bomb = ct.rect( x, y, width, height, color );
+		double speed = 0.5;
+		// Start the duck moving to the right
+		duck.setXSpeed( speed );
+		// Start the bomb moving down
+		bomb.setYSpeed( speed );
+		// Start the bullet moving up
+		bullet.setYSpeed( -speed );
 	}
 
 	public void update()
 	{
-		double speed = 0.5;
-		// Move the duck to the right, wrapping around at edge of screen
-		duck.x = ( duck.x + speed ) % 100;
-		// Move the bomb down
-		bomb.y += speed;
-		// Move the bullet up
-		bullet.y -= speed;
 	}
 }
