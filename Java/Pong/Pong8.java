824a01baee225806e6803dc80fb72dc38fe3f4b6
Ben Bennett-Cauchon <bcauchon@gmail.com>
2019-01-02 16:59:07 -0800

    Pong upto syntax level 12
    -- Added some summary comments to top of Pong 1-9.java
    -- Modified Pong8&9.java so that they both allow a complete game to be played.
    -- Added Pong10-12.java

--- Java/Pong/Pong8.java
+++ Java/Pong/Pong8.java
@@ -1,17 +1,26 @@
 // Pong8.java
 // If-else
+// A playable two-player Pong-like game.
+// w and s keys control the left paddle, up and down keys control the right paddle.
 
 class Pong8
 {
 	final double WIDTH = 100;             // Graphics output window width
 	final double HEIGHT = WIDTH / 1.5;    // Graphics output window height
 	final double SPEED = 1;               // Speed of the paddles
-	GameObj leftScore;   // Left score display
-	GameObj rightScore;  // Right score dispaly
+	final double MAX_SCORE = 15;          // Score needed to win the game
+	int leftScore;              // Left player's score
+	int rightScore;             // Right player's score
+	GameObj leftScoreDisplay;   // Left player's score display
+	GameObj rightScoreDisplay;  // Right player's score dispaly
 	GameObj leftPaddle;  // Left player paddle
 	GameObj rightPaddle; // Right player paddle
 	GameObj ball;        // Game ball
 	boolean ballInPlay;  // True when the ball is in play
+	boolean firstServe;    // True the first time the ball is served
+	int directionAngle;    // Angle from horizontal for the ball's path
+	GameObj instructions;  // Text for showing gameplay instructions
+	GameObj endGameMsg;    // Text for showing end of game message
 
 	public void start()
 	{
@@ -32,10 +41,10 @@ class Pong8
 		double scoreOffset = 25;
 		double x = WIDTH / 2 - scoreOffset;
 		double y = scoreHeight / 2;		
-		leftScore = ct.text( text, x, y, scoreHeight, color );
+		leftScoreDisplay = ct.text( text, x, y, scoreHeight, color );
 		// Make right score display
 		x = WIDTH / 2 + scoreOffset;
-		rightScore = ct.text( text, x, y, scoreHeight, color );
+		rightScoreDisplay = ct.text( text, x, y, scoreHeight, color );
 		// Make left paddle
 		double paddleMargin = 10;
 		x = paddleMargin;
@@ -46,19 +55,27 @@ class Pong8
 		// Make right paddle
 		x = WIDTH - paddleMargin;
 		rightPaddle = ct.rect( x, y, paddleWidth, paddleHeight, color );
-		// Make ball next to left paddle
-		double diameter = paddleWidth;
-		x = leftPaddle.x + leftPaddle.getWidth() / 2 + diameter / 2;
+		// Make ball
+		x = 0;
+		y = 0;
+		double diameter = 2;
 		ball = ct.circle( x, y, diameter, color );
-		// Set ball speed and direction
-		double ballSpeed = 0.7;
-		int directionAngle = ct.random( -15, 15 ); 
-		double xSpeed = ballSpeed * Math.cos( directionAngle * Math.PI / 180 );
-		double ySpeed = ballSpeed * Math.sin( directionAngle * Math.PI / 180 );
-		// Start ball moving
-		ball.setXSpeed( xSpeed );
-		ball.setYSpeed( ySpeed );
-		ballInPlay = true;
+		ball.visible = false; 
+		// Make instructions display
+		text = "Press space to serve the ball";
+		x = WIDTH / 2; 
+		y = HEIGHT / 2;
+		double h = 5;
+		color = "yellow";
+		instructions = ct.text( text, x, y, h, color );
+		// Make end of game display
+		y = instructions.y - instructions.getHeight() * 2 ;
+		endGameMsg = ct.text( "", x, y, h, color );
+		// Initialize game state variables
+		leftScore = 0;
+		rightScore = 0;
+		ballInPlay = false;
+		firstServe = true;
 	}
 
 	public void update()
@@ -90,22 +107,80 @@ class Pong8
 				ball.setXSpeed( -ball.getXSpeed() );
 
 			// Update the score if the ball goes off the right or left edge of the game window
-			if ( ball.x > WIDTH + ball.getWidth() / 2 )
+			boolean ballOffRightEdge = ball.x > WIDTH + ball.getWidth() / 2;
+			boolean ballOffLeftEdge = ball.x < -ball.getWidth() / 2;
+			if ( ballOffRightEdge || ballOffLeftEdge )
 			{
-				// Ball went off right edge
-				// Update leftScore
-				int newScore = ct.parseInt( leftScore.getText() ) + 1;
-				leftScore.setText( "" + newScore );
+				// Update score
+				if ( ballOffRightEdge )
+				{
+					leftScore++;
+					leftScoreDisplay.setText( "" + leftScore );
+
+				}
+				else
+				{
+					rightScore++;
+					rightScoreDisplay.setText( "" + rightScore );
+				}
+				// Check for a win on either side
+				if ( leftScore == MAX_SCORE || rightScore == MAX_SCORE )
+				{
+					firstServe = true;
+					endGameMsg.visible = true;
+					if ( leftScore == MAX_SCORE )
+						endGameMsg.setText( "Left Player Wins!" );
+					else
+						endGameMsg.setText( "Right Player Wins!" );
+				}
+				// Prep for next serve
 				ballInPlay = false;
+				directionAngle = ct.round( Math.atan2( ball.getYSpeed(), ball.getXSpeed() ) * 180 / Math.PI );
+				// Stop the ball moving so that it doesn't get auto deleted
+				ball.setXSpeed( 0 );
+				ball.setYSpeed( 0 );
+				// Show the instructions for the next serve
+				instructions.visible = true;
 			}
-			else if ( ball.x < -ball.getWidth() / 2 )
+		}
+		else if ( ct.keyPressed( "space" ) )
+		{
+			// Hide instructions
+			instructions.visible = false;
+			// Set ball speed and direction
+			double ballSpeed = 1;
+			if ( firstServe )
 			{
-				// Ball went off left edge
-				// Update rightScore
-				int newScore = ct.parseInt( rightScore.getText() ) + 1;
-				rightScore.setText( "" + newScore );
+				// Initialize game state variables
+				leftScore = 0;
+				rightScore = 0;
 				ballInPlay = false;
+				// Set score displays
+				leftScoreDisplay.setText( "0" );
+				rightScoreDisplay.setText( "0" );
+				// Hide end of game text
+				endGameMsg.visible = false;
+				// Give the ball a random direction
+				int spread = 15; // Max number of degrees up/down from horizontal the ball will be served
+				int direction = ct.random( 1, 2 );
+				if ( direction == 1 )
+					// Serve the ball to the right
+					directionAngle = ct.random( -spread, spread );
+				else
+					// Serve the ball to the left
+					directionAngle = ct.random( 180 - spread, 180 + spread );
+
+				firstServe = false;
+				ball.visible = true;
 			}
+			// Set the ball's x- and y-speeds
+			ball.setXSpeed( ballSpeed * Math.cos( directionAngle * Math.PI / 180 ) );
+			ball.setYSpeed( ballSpeed * Math.sin( directionAngle * Math.PI / 180 ) );
+			// Set the ball at the center of the screen
+			ball.x = WIDTH / 2;
+			ball.y = HEIGHT / 2;
+			// Set ball in play
+			ballInPlay = true;
 		}
 	}
 }
